5b9dd3c696c22117471d7985d1f029308f9d628d
Pushpak Raut <85230759+PushpakRaut@users.noreply.github.com>
2023-09-18 21:43:49 +0530

    Create 572-Matrix-Diagonal-Sum.cpp

---
+++ Array/1572-Matrix-Diagonal-Sum/572-Matrix-Diagonal-Sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int i=0, j=0;
+        int n = mat.size();
+        int sum = 0;
+        while(i<n && j < n) {
+            sum += mat[i][j];
+            mat[i][j] = 0;
+            i++;
+            j++;
+        }
+        j-=1;
+        i=0;
+        while(j>=0 && i<n){
+            sum += mat[i][j];
+            mat[i][j] = 0;
+            j--;
+            i++;
+        }
+
+        return sum;
+    }
+};
