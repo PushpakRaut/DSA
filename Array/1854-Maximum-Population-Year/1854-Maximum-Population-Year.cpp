d3166cf88146bb4fc77aeb498f2afeea7f86e415
Pushpak Raut <85230759+PushpakRaut@users.noreply.github.com>
2023-09-23 17:23:31 +0530

    Create 1854-Maximum-Population-Year.cpp

---
+++ Array/1854-Maximum-Population-Year/1854-Maximum-Population-Year.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumPopulation(vector<vector<int>>& logs) {
+        int arr[101] = {0};
+
+        for(auto log : logs) {
+            for(int j=log[0];j<log[1];j++) {
+                arr[j-1950]++;
+            }
+        }
+
+        int maxYear = 1950, max = 0;
+        for(int i = 0; i<101;i++) {
+            if(max < arr[i]) {
+                max = arr[i];
+                maxYear = i + 1950;
+            }
+        }
+
+        return maxYear;
+    }
+};
